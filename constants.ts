e441422e59c2a8e3314aa4c4a7743694963dd85f
Nguyễn Lê Đăng Khoa <nguyenledangkhoa146205@gmail.com>
2026-01-26 22:45:31 +0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -3,3 +3,9 @@ export const API_BASE_URL = '/nldk/Exam';
 export const SESSION_KEY = 'exam_ongoing_session';
 export const CACHE_KEY_SUBJECTS = 'danh_sach_mon_hoc';
 export const CACHE_TIME = 1000 * 60 * 20; // 20 minutes
+
+// Cấu hình Header cho các yêu cầu API
+export const API_HEADERS = {
+  'Content-Type': 'application/json',
+  'x-api-key': 'NLDK@146' // Bạn có thể thay đổi key này theo yêu cầu của Backend
+};
