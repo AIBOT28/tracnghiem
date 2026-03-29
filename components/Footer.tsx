f10cd15ce3a67d6fdefae445658bcab7f11a9cc2
AIBOT28 <nguyenledangkhoa14062005@gmail.com>
2026-03-29 22:10:07 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,6 +18,7 @@ const Footer: React.FC<FooterProps> = ({ visitorCount }) => {
       <div className="mt-1 text-xs text-gray-400 flex justify-center items-center gap-1">
         <i className="fa-solid fa-eye"></i> Lượt truy cập: <span className="font-bold text-gray-500">{visitorCount}</span>
       </div>
+      <script id="_wau0u3">var _wau = _wau || []; _wau.push(["small", "u67wa694cy", "0u3"]);</script><script async src="//waust.at/s.js"></script>
     </div>
   );
 };
