1f4a5fdd9bf61fb4717a468d6f3092f6dfde6a85
nguye <nguye@LAPTOP-KHNRIK62>
2026-05-04 22:28:44 +0700

    Setup Cloudflare Pages and update API URLs

--- constants.ts
+++ constants.ts
@@ -1,9 +1,11 @@
-export const API_BASE_URL = '/nldk/Exam';
-export const AUTH_API_URL = '/api/Auth'; // URL của backend API cho auth
-export const MONHOC_API_URL = '/api/MonHocs';
-export const CHUONG_API_URL = '/api/Chuongs';
-export const CAUHOI_API_URL = '/api/CauHois';
-export const WS_URL = '/visitorHub';
+const BASE_URL = 'https://backend.nguyenledangkhoa146205.workers.dev';
+
+export const API_BASE_URL = `${BASE_URL}/nldk/Exam`;
+export const AUTH_API_URL = `${BASE_URL}/api/Auth`; // URL của backend API cho auth
+export const MONHOC_API_URL = `${BASE_URL}/api/MonHocs`;
+export const CHUONG_API_URL = `${BASE_URL}/api/Chuongs`;
+export const CAUHOI_API_URL = `${BASE_URL}/api/CauHois`;
+export const WS_URL = `${BASE_URL}/visitorHub`;
 export const SESSION_KEY = 'exam_ongoing_session';
 export const CACHE_KEY_SUBJECTS = 'danh_sach_mon_hoc';
 export const CACHE_KEY_CHAPTERS = 'danh_sach_chuong_';
